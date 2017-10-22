18b9fbd71c43dfc892041a5b6b820f61f44167b8
meen <59030143@kmitl.ac.th>
2017-10-23 02:07:59 +0700

    w09 r28 hello world

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -22,19 +22,11 @@ namespace GDIPlus_1
             Graphics g = e.Graphics;
             Bitmap bmp = new Bitmap("D:\\Capture.PNG");
             this.SetClientSizeCore(bmp.Width, bmp.Height);
-            Rectangle topLeft = new Rectangle(0, 0, bmp.Width / 2, bmp.Height / 2);
-            Rectangle topRight = new Rectangle(bmp.Width / 2, 0, bmp.Width / 2, bmp.Height / 2);
-            Rectangle bottomLeft = new Rectangle(0, bmp.Height / 2, bmp.Width / 2, bmp.Height / 2);
-            Rectangle bottomRight = new Rectangle(bmp.Width / 2, bmp.Height / 2, bmp.Width / 2, bmp.Height / 2);
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
-            e.Graphics.DrawImage(bmp, topLeft);
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
-            e.Graphics.DrawImage(bmp, topRight);
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
-            e.Graphics.DrawImage(bmp, bottomLeft);
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
-            e.Graphics.DrawImage(bmp, bottomRight);
-
+            Rectangle destRect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            Brush myBrush = new SolidBrush(Color.Coral);
+            g.DrawImage(bmp, destRect);
+            g.DrawString("Hello world",
+            new Font("Verdana", 30, FontStyle.Bold), myBrush, 0, 0);
             g.Dispose();
         }
 
